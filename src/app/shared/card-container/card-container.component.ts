ede7c2405f0f0b2ccbe8d6c49876f1a8eaf8a63d
auffaj <65475096+auffaj@users.noreply.github.com>
2024-08-30 20:38:36 -0400

    updated spells with card container

--- src/app/shared/card-container/card-container.component.ts
+++ src/app/shared/card-container/card-container.component.ts
@@ -16,9 +16,10 @@ import { NgIf } from '@angular/common';
   imports: [MatSidenavModule, MatButtonModule,MatIconModule,NgIf]
 })
 export class CardContainerComponent {
-  @Input() description: string | null = null;
+  @Input() description: string[] | string | null = null;
   @Input() tags: string[] | string | null = null;
   @Input() sourcebook: string | null = null;
+  @Input() classes: string[] | null = null;
 
   public currentSubject: string | null = null;
   public title: string | null = null;
@@ -41,11 +42,21 @@ export class CardContainerComponent {
   getDrawerContent(): {title: string | null, body: string | null}{
     switch(this.currentSubject){
       case("description"): 
-        return {title: null, body: this.description == null ? "" : this.description};
+        let _description_ = "";
+
+        if(typeof(this.description) == "string"){
+          _description_ = this.description as string;
+        } else if(this.description != null){
+          _description_ = this.description.join("\n\n")
+        }
+                             
+        return {title: null, body: _description_};
       case("tags"): 
         return {title: "Tags:", body: this.tags == null ? "" : this.tags.toString()};
       case("sourcebook"):
         return {title: "Source Book", body: this.sourcebook == null ? "" : this.sourcebook};
+      case("classes"):
+        return {title: "Classes", body: this.classes == null ? "" : this.classes.join(", ")}
       default: 
         return {title: null, body: null};
     }
