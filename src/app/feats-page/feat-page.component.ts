b43ac91a4b7d4d67dd4acac30ab6b7ade57bf78f
auffaj <65475096+auffaj@users.noreply.github.com>
2024-09-01 15:51:08 -0400

    updated feats with search bar component

--- src/app/feats-page/feat-page.component.ts
+++ src/app/feats-page/feat-page.component.ts
@@ -4,41 +4,59 @@ import { CommonModule } from '@angular/common';
 import { FeatCardComponent } from './components/feat-card/feat-card.component';
 import { HttpClient } from '@angular/common/http';
 import { CardContainerComponent } from '../shared/card-container/card-container.component';
+import { SearchBarFeatComponent } from '../shared/search-bar/search-bar.component';
+import { SearchService } from '../shared/services/search/search.service';
 
 @Component({
   selector: 'fiveE-archive-feats-page',
   standalone: true,
-  imports: [CommonModule, FeatCardComponent, CardContainerComponent],
+  imports: [CommonModule, FeatCardComponent, CardContainerComponent, SearchBarFeatComponent],
   templateUrl: './feat-page.component.html',
   styleUrl: './feat-page.component.scss'
 })
 export class FeatsPageComponent implements OnInit {
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private search: SearchService){}
   public feats: Feat[] = []
-  private pristineFeats: Feat[] = []
-
+  private debounce: any = null;
+  
   ngOnInit(){
     this.http.get('assets/feats.json', {responseType: 'json'})
         .subscribe(data => {
-          this.pristineFeats = JSON.parse(JSON.stringify(data));
-          this.setShownSpells(data as Feat[]);
+          this.search.setData(data);
+          this.setShownFeats(data as Feat[]);
         });
      
   }
 
-  setShownSpells(_data_: Feat[]){
+  setShownFeats(_data_: Feat[]){
     this.feats = JSON.parse(JSON.stringify(_data_));
   }
 
-  filterSearched(event: Event){
-
-    const searchTerm: string = (event.target as HTMLInputElement).value.toLowerCase();
-
-    if(searchTerm == ''){
-      this.setShownSpells(this.pristineFeats);
-    } else {
-      const newSpells = this.pristineFeats.filter(row => row.name.toLowerCase().indexOf(searchTerm) >= 0)
-      this.setShownSpells(newSpells);
+  /**
+    Queue search with debounce
+  */
+    filterSearched(event: Event){
+      if(this.debounce != null){
+        clearTimeout(this.debounce);
+      }
+  
+      this.debounce = setTimeout(() => this.doFilterSearch(event), 150);
     }
-  }
+  
+    private doFilterSearch(event: Event){
+      const nameSearchTerm: string = event.toString().toLowerCase();
+  
+      const params: {searchKey: string, searchValue: any}[] = []
+  
+      if(!!nameSearchTerm){
+  
+        params.push({
+          searchKey:"name",
+          searchValue: nameSearchTerm
+        })
+      }
+      
+      this.debounce = null;
+      this.setShownFeats(this.search.getSearchResults(params) as Feat[]);
+    }
 }
