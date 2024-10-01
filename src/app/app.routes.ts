d36ebc54574130b2db1fe7e3c5490eac79b8900f
auffaj <65475096+auffaj@users.noreply.github.com>
2024-09-30 22:42:09 -0400

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { QuickReferenceComponent } from './quick-reference/quick-reference.component';
+import { SplashPageComponent } from './splash-page/splash-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'quick-reference',
+    component:QuickReferenceComponent
+},{
+    path:'landing',
+    component: SplashPageComponent
+},{
+    path:'',
+    redirectTo:"/landing",
+    pathMatch:"full"
+}];
--- src/app/layout/sidenav/sidenav.component.ts
+++ src/app/layout/sidenav/sidenav.component.ts
@@ -1,15 +1,34 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatTooltip } from '@angular/material/tooltip'
+import { Router, RouterLink } from '@angular/router';
+
+interface SideNavButton {
+  tooltip: string, icon: string, url: string
+}
 
 @Component({
   selector: 'fiveE-archive-sidenav',
   standalone: true,
-  imports: [MatIcon, MatTooltip, CommonModule],
+  imports: [MatIcon, MatTooltip, CommonModule,RouterLink, NgFor],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
+  constructor(private router: Router){}
 
+  public buttons : SideNavButton[] = [{
+    tooltip:"Landing Page",
+    url:"/landing",
+    icon:"home"
+  },{
+    tooltip:"Spells, Equipment, Feats and Magic Items",
+    url:"/quick-reference",
+    icon:"manage_search"
+  }]
+
+  getClass(button: SideNavButton){
+    return {"nav-button":true, "selected": this.router.url == button.url};
+  }
 }
