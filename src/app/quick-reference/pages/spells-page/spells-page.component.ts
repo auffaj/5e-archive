31f5c6f206d40434a5e7120a6e4d75e4f6f19c0a
auffaj <65475096+auffaj@users.noreply.github.com>
2024-10-01 22:51:25 -0400

    included as part of previous commit

--- src/app/quick-reference/pages/spells-page/spells-page.component.ts
+++ src/app/quick-reference/pages/spells-page/spells-page.component.ts
@@ -7,11 +7,13 @@ import { SearchBarSpellComponent } from '../../../shared/search-bar/search-bar.c
 import { SearchService } from '../../../shared/services/search/search.service';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { DataService } from '../../../shared/services/data/data.service';
+import { MatButton } from '@angular/material/button';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
   selector: 'fiveE-archive-spells-page',
   standalone: true,
-  imports: [CommonModule, SpellCardComponent, CardContainerComponent, SearchBarSpellComponent, MatProgressBar],
+  imports: [CommonModule, SpellCardComponent, CardContainerComponent, SearchBarSpellComponent, MatProgressBar, MatButton, MatTooltip],
   providers:[DataService],
   templateUrl: './spells-page.component.html',
   styleUrl: './spells-page.component.scss'
@@ -20,10 +22,10 @@ export class SpellsPageComponent implements OnInit {
   constructor(private search: SearchService, private data: DataService){}
   public cards: Spell[] = []
   private debounce: any = null;
-  private maxSize: number = 0;
   public limitCount: number = 20;
 
   public loading: boolean = true;
+  private filter: any;
 
   ngOnInit(){
     this.data.getSpells()
@@ -41,10 +43,16 @@ export class SpellsPageComponent implements OnInit {
     this.cards = _data_.slice(0, this.limitCount);
   }
 
-  addMore(){
-    this.limitCount += 25;
-    
-    this.setShownCards(this.search.getSearchResults() as Spell[]);
+  addMore(amountToAdd: number | null = null){
+    const myData: Spell[] = this.search.getSearchResults(this.filter) as Spell[];
+
+    this.limitCount = amountToAdd  == null ?  myData.length : this.limitCount + amountToAdd;
+        
+    myData.slice(0, this.limitCount).forEach(spell => {
+      if(this.cards.find(card => card.name == spell.name) == undefined){
+        this.cards.push(spell)
+      }
+    })
   }
 
   /**
@@ -60,6 +68,7 @@ export class SpellsPageComponent implements OnInit {
   
     private doFilterSearch(event: Spell){
       this.debounce = null;
+      this.filter = event;
       this.setShownCards(this.search.getSearchResults(event) as Spell[]);
     }
 }
